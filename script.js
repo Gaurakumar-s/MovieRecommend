0d2fa592240918f410b2c7789b7782325ddc4eda
Gaurav <Student-sst@outlook.com>
2025-07-06 09:20:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // API Keys
 const TMDB_KEY = '3fd2be6f0c70a2a598f084ddfb75487c';
-const GEMINI_KEY = 'AIzaSyAtsN7DlHplSbEZCqND1HkWAAkd_MpNZeU';
+const GEMINI_KEY = 'AIzaSyCaC1SNEVeMj9IM15Rcj-Nzi7iygCH00iY';
 
 // DOM Elements
 const searchInput = document.getElementById('search');
